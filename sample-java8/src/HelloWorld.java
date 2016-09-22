c951ae900a3191163769f9a524ec7aa4d59779df
niranjan.kachhawa <niranjan.kachhawa@IN-IT0055.globant.com>
2016-09-22 12:49:05 +0530

    Changes

--- sample-java8/src/HelloWorld.java
+++ sample-java8/src/HelloWorld.java
@@ -3,5 +3,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Jenkins");
+		System.out.println("                  **  ");
+		System.out.println("                ****  ");
+		System.out.println("              ******  ");
+		System.out.println("            ********  ");
+		System.out.println("          **********  ");
+		System.out.println("        ************  ");
+		System.out.println("      **************  ");
+		System.out.println("    ****************  ");
+		
 	}
 }
